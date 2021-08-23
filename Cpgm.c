28cf3d87162170073ecfbfbbdb5d5740990f64b4
anuragdandge <41123540+anuragdandge@users.noreply.github.com>
2021-08-23 23:09:49 +0530

    Pgm 13 , 14 & 15 Added

--- Cpgm.c
+++ Cpgm.c
@@ -1,13 +1,25 @@
+
 #include <stdio.h>
 void main()
 {
-  char ch ;
-   printf ("\n Enter a Character : \n ");
-   scanf("%c",&ch);
-   if(ch == 'a' || ch == 'A' ||ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' ||ch == 'o' || ch == 'O'||ch == 'u' || ch == "U" ){
-     printf(" %c is Vowel ",ch );
-   }
-   else{
-     printf("\n  %c ic Consonant ",ch);
-   }
+   int salary;
+   printf("Enter the salary: ");
+   scanf("%d", &salary);
+   if (salary >= 100)
+    {
+      if(salary > 100)
+      {
+        salary = 4 * salary + 100;
+        printf("\n  salary is %d", salary); 
+      }
+      else{
+        salary = 300;
+        printf("\n  salary is %d", salary);
+      }
+    }
+    else
+    {
+      salary = 4.5 * salary + 150;
+      printf("\n  salary is %d", salary);
+    }
 }
