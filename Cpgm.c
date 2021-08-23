9d2ded3371c75b68a32d400b45d3ab0b77f7e0bc
anuragdandge <41123540+anuragdandge@users.noreply.github.com>
2021-08-23 18:24:10 +0530

    Added pgm 11 & 12

--- Cpgm.c
+++ Cpgm.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
-void main(){            
-   int num;
-  printf("\n Enter a number : \t ");
-  scanf("%d",&num);
-  if(num % 2 == 0 ){
-    printf("\n  %d is Even number .",num );
-  }
-  else{
-    printf("\n \t %d is Odd number .",num);
-  }
+#include <stdio.h>
+void main()
+{
+  char ch ;
+   printf ("\n Enter a Character : \n ");
+   scanf("%c",&ch);
+   if(ch == 'a' || ch == 'A' ||ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' ||ch == 'o' || ch == 'O'||ch == 'u' || ch == "U" ){
+     printf(" %c is Vowel ",ch );
+   }
+   else{
+     printf("\n  %c ic Consonant ",ch);
+   }
 }
